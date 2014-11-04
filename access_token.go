23ccc67c73d8221f76e0ebced4012aa2afe86452
Jimmy Sawczuk <github@jimmysawczuk.com>
2014-11-04 13:36:55 -0500

    Moving some stuff around

--- access_token.go
+++ access_token.go
@@ -23,3 +23,41 @@ func (at AccessToken) Empty() bool {
 func (at AccessToken) Valid() bool {
 	return at.valid
 }
+
+// Figures out what permissions are attached to the current access token.
+func (at *AccessToken) Lint() error {
+	if at.token == "" {
+		return fmt.Errorf("Access token not set")
+	}
+
+	result, err := f.api("/debug_token", Get, map[string]interface{}{
+		"input_token":  at.token,
+		"access_token": f.appId + "|" + f.secret,
+	})
+
+	if err == nil {
+		if _, exists := result["data"]; exists {
+			data := result["data"].(map[string]interface{})
+
+			if valid, exists := data["is_valid"]; exists {
+				at.valid = valid.(bool)
+			}
+
+			if expires, exists := data["expires_at"]; exists {
+				at.expires = time.Unix(int64(expires.(float64)), 0)
+			}
+
+			if _, exists := data["scopes"]; exists {
+				perms := data["scopes"].([]interface{})
+				at.permissions = []string{}
+				for _, v := range perms {
+					at.permissions = append(at.permissions, fmt.Sprintf("%s", v))
+				}
+			}
+		}
+
+		f.accessToken = at
+	}
+
+	return err
+}
--- facebook.go
+++ facebook.go
@@ -54,46 +54,6 @@ func (f *Client) GetAppAccessToken() (string, error) {
 	}
 }
 
-// Figures out what permissions are attached to the current access token.
-func (f *Client) LintAccessToken() error {
-	at := f.accessToken
-
-	if at.token == "" {
-		return fmt.Errorf("Access token not set")
-	}
-
-	result, err := f.api("/debug_token", Get, map[string]interface{}{
-		"input_token":  at.token,
-		"access_token": f.appId + "|" + f.secret,
-	})
-
-	if err == nil {
-		if _, exists := result["data"]; exists {
-			data := result["data"].(map[string]interface{})
-
-			if valid, exists := data["is_valid"]; exists {
-				at.valid = valid.(bool)
-			}
-
-			if expires, exists := data["expires_at"]; exists {
-				at.expires = time.Unix(int64(expires.(float64)), 0)
-			}
-
-			if _, exists := data["scopes"]; exists {
-				perms := data["scopes"].([]interface{})
-				at.permissions = []string{}
-				for _, v := range perms {
-					at.permissions = append(at.permissions, fmt.Sprintf("%s", v))
-				}
-			}
-		}
-
-		f.accessToken = at
-	}
-
-	return err
-}
-
 func NewGraphError(data map[string]interface{}) (fge GraphError, err error) {
 	b, _ := json.Marshal(data)
 	err = json.Unmarshal(b, &fge)
